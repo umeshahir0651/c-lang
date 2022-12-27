88cb72b876c0c29e1441f95dd590a7099707da48
umeshahir0651 <umeshahir0964@gmail.com>
2022-12-27 17:39:36 +0530

    c language programs

---
+++ INTRO.C
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ #include<conio.h>
+ void main()
+ {
+ clrscr();
+
+ printf("my name is umesh ahir\n");
+ printf("i live in ahmedabad\n");
+ printf("my birth date 14/7/2004\n");
+ printf("now i am doing c++ cources\n");
+
+
+ getch();
+ }
---
+++ PR1.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+
+{
+float pi=3.14,r,area;
+clrscr();
+
+ printf("enter radious of circle");
+ scanf("%d",&r);
+ area=pi*r*r;
+ printf("area of circle of %.2",area);
+
+
+
+
+ getch();
+
+ }
+
